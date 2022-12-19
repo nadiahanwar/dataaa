d4b7e6643321a2e79804e0679e4515b059949f5d
nadiahanwar <120996690+nadiahanwar@users.noreply.github.com>
2022-12-19 10:42:16 +0800

    Update myfirstapp.py

--- myfirstapp.py
+++ myfirstapp.py
@@ -1,21 +1,4 @@
-import streamlit as st
-import numpy as np
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-
-st.header("My first Streamlit App")
-st.write(pd.DataFrame({
-    'Intplan': ['yes', 'yes', 'yes', 'no'],
-    'Churn Status': [0, 0, 0, 1]
-}))
-
-iris = sns.load_dataset('iris')
-fig = plt.figure(figsize=(10, 4))
-sns.boxplot(data=iris)
-st.pyplot(fig)
-Academy Trainer10:28 AM
-import streamlit as st
+mport streamlit as st
 import numpy as np
 import pandas as pd
 import seaborn as sns
